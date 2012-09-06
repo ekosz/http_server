38a4937e163c71b62b22bb37d30742015ca183d3
Eric Koslow <ekoslow@gmail.com>
2012-09-06 15:06:55 -0500

    Initial Commit

---
+++ src/httpserver/Parser.java
@@ -0,0 +1,84 @@
+package httpserver;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ekoslow
+ * Date: 9/5/12
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parser {
+
+    private String uri;
+    private String version;
+    private String verb;
+    private String body;
+    private HashMap<String, String> headers = new HashMap<String, String>();
+
+    public Parser(String request) {
+        String lines[] = request.split("\\r?\\n");
+        parseInitialLine(lines[0]);
+        parseHeaders(Arrays.copyOfRange(lines, 1, lines.length));
+    }
+
+    private void parseHeaders(String[] strings) {
+        if(strings.length == 0) return;
+        Integer i = 0;
+        String line = strings[i];
+        while (!Pattern.matches("^\\s*$", line) && i < strings.length) {
+            String keyVal[] = line.split(":");
+            this.headers.put(keyVal[0].toLowerCase(), keyVal[1].trim());
+            line = strings[i++];
+        }
+        if(i == strings.length) return;
+        parseBody(Arrays.copyOfRange(strings, ++i, strings.length));
+    }
+
+    private void parseBody(String[] strings) {
+        this.body = combine(strings, "\n");
+    }
+
+    private void parseInitialLine(String line) {
+        String sections[] = line.split(" ");
+        this.verb = sections[0].toLowerCase();
+        this.uri = sections[1];
+        this.version = sections[2];
+    }
+
+    public String verb() {
+        return verb;
+    }
+
+    public String uri() {
+        return uri;
+    }
+
+    public String version() {
+        return version;
+    }
+
+    public HashMap<String, String> headers() {
+        return headers;
+    }
+
+    public String body() {
+        return body;
+    }
+
+    private String combine(String[] s, String glue) {
+        int k = s.length;
+        if (k == 0)
+            return null;
+        StringBuilder out = new StringBuilder();
+        out.append(s[0]);
+        for (int x = 1; x < k; ++x)
+            out.append(glue).append(s[x]);
+        return out.toString();
+    }
+}
---
+++ test/httpserver/ParserTest.java
@@ -0,0 +1,56 @@
+package httpserver;
+
+import org.junit.*;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ekoslow
+ * Date: 9/5/12
+ * Time: 3:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParserTest {
+
+    @Test
+    public void readTheCorrectVerb() throws Exception {
+      assertEquals("get", new Parser("GET /index.html HTTP/1.1").verb());
+    }
+
+    @Test
+    public void readTheCorrectUri() throws Exception {
+      assertEquals("/", new Parser("GET / HTTP/1.1").uri());
+    }
+
+    @Test
+    public void readTheCorrectVersion() throws Exception {
+      assertEquals("HTTP/1.1", new Parser("GET /index.html HTTP/1.1").version());
+    }
+
+    @Test
+    public void createsTheCorrectHeaders() throws Exception {
+      String request = "GET / HTTP/1.1\r\nHost: google.com";
+      assertEquals("google.com", new Parser(request).headers().get("host"));
+    }
+
+    @Test
+    public void createsMultiableHeaders() throws Exception {
+      String request = "GET / HTTP/1.1\r\nHost: google.com\r\nContent-Length: 10";
+      assertEquals("google.com", new Parser(request).headers().get("host"));
+      assertEquals("10", new Parser(request).headers().get("content-length"));
+    }
+
+    @Test
+    public void createsTheProperBody() throws Exception {
+      String request = "GET / HTTP/1.1\r\n\r\nBODY";
+      assertEquals("BODY", new Parser(request).body());
+    }
+
+    @Test
+    public void fullHttpRequest() throws Exception {
+      String request = "GET /hi HTTP/1.1\nHost: localhost:3000\nConnection: keep-alive\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.57 Safari/537.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\nAccept-Encoding: gzip,deflate,sdch\nAccept-Language: en-US,en;q=0.8\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\n\n\n";
+
+      assertEquals("get", new Parser(request).verb());
+    }
+}
